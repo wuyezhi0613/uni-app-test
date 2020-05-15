8d98f54eb5c51b4bdbbed1ebd1d02249bf1bea5d
wuyz <1176331954@qq.com>
2020-05-15 17:05:40 +0800

    feat: 添加框架

---
+++ pages/index/header.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<uni-nav-bar left-icon="back" left-text="返回" right-text="菜单" title="导航栏组件"></uni-nav-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "header",
+		components: {
+			header
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<uni-nav-bar left-icon="back" left-text="返回" right-text="菜单" title="导航栏组件"></uni-nav-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
